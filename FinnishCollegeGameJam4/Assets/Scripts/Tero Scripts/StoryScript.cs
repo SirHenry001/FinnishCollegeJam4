707b4ef174ca69593f14922ae83dad0fddd553aa
Kirjoittamaton <godofthemindx@gmail.com>
2023-03-30 10:36:03 +0300

    animaatioiden lisäys
    animaatioiden lisäys

--- FinnishCollegeGameJam4/Assets/Scripts/Tero Scripts/StoryScript.cs
+++ FinnishCollegeGameJam4/Assets/Scripts/Tero Scripts/StoryScript.cs
@@ -1,11 +1,15 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.IO;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
 
+
 public class StoryScript : MonoBehaviour
 {
+   
+
     public GameObject Text1;
     public GameObject Text2;
     public GameObject Text3;
@@ -22,13 +26,13 @@ public class StoryScript : MonoBehaviour
     {
         yield return new WaitForSecondsRealtime(1f);
         Text1.SetActive(true);
-        yield return new WaitForSecondsRealtime(1f);
+        yield return new WaitForSecondsRealtime(2f);
         Text2.SetActive(true);
-        yield return new WaitForSecondsRealtime(1f);
+        yield return new WaitForSecondsRealtime(3f);
         Text3.SetActive(true);
-        yield return new WaitForSecondsRealtime(1f);
+        yield return new WaitForSecondsRealtime(4f);
         Text4.SetActive(true);
-        yield return new WaitForSecondsRealtime(1f);
+        yield return new WaitForSecondsRealtime(6f);
         SceneManager.LoadScene(2);
 
     }
